d86b8b0ae7410d2606cf490bf1299981fd6b4fb8
Shinichi Katayama <shin1.katayama@gmail.com>
2022-01-31 21:16:17 -0800

    Use TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,35 @@
 import 'package:flutter/material.dart';
-import 'exports.dart';
 
-import 'DummySyntaxHighlighter.dart';
+void main() => runApp(const MoonApp());
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
+class MoonApp extends StatelessWidget {
+  const MoonApp({Key? key}) : super(key: key);
 
-class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => DemoCodeEditor()));
-            },
-            child: Text("My Code Editor"),
-          ),
-        ),
+    const appTitle = 'Moon';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        body: CodeEditor(),
       ),
     );
   }
 }
 
-class DemoCodeEditor extends StatefulWidget {
-  @override
-  _DemoCodeEditorState createState() => _DemoCodeEditorState();
-}
-
-class _DemoCodeEditorState extends State<DemoCodeEditor> {
-  RichCodeEditingController? _rec;
-  SyntaxHighlighterBase? _syntaxHighlighterBase;
-
-  @override
-  void initState() {
-    super.initState();
-    _syntaxHighlighterBase = DummySyntaxHighlighter();
-    _rec = RichCodeEditingController(_syntaxHighlighterBase, text: '');
-  }
-
-  @override
-  void dispose() {
-    //_richTextFieldState.currentState?.dispose();
-    super.dispose();
-  }
+class CodeEditor extends StatelessWidget {
+  const CodeEditor({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Dummy Editor"),
+    return Scaffold(
+      body: SizedBox.expand(
+        child: TextField(
+          decoration: null,
+          keyboardType: TextInputType.multiline,
+          maxLines: null,
+        ),
       ),
-      body: Container(
-          height: 300.0,
-          margin: EdgeInsets.all(24.0),
-          padding: EdgeInsets.all(24.0),
-          decoration:
-              new BoxDecoration(border: new Border.all(color: Colors.grey)),
-          child: RichCodeField(
-            autofocus: true,
-            controller: _rec,
-            textCapitalization: TextCapitalization.none,
-            decoration: null,
-            syntaxHighlighter: _syntaxHighlighterBase,
-            maxLines: null,
-            onChanged: (String s) {},
-            onBackSpacePress: (TextEditingValue oldValue) {},
-            onEnterPress: (TextEditingValue oldValue) {
-              var result = _syntaxHighlighterBase!.onEnterPress(oldValue);
-              if (result != null) {
-                _rec!.value = result;
-              }
-            },
-          )),
     );
   }
 }
