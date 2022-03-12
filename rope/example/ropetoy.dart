e433c4dcbf4146855c9d2d50f8e8da1d576fa4fd
Shinichi Katayama <shin1.katayama@gmail.com>
2022-03-11 22:42:26 -0800

    Update ropetoy

--- rope/example/ropetoy.dart
+++ rope/example/ropetoy.dart
@@ -3,19 +3,19 @@ import 'package:rope/rope.dart';
 // Toy app for experimenting with ropes
 void main() {
   var s = Stopwatch()..start();
-  RopeNode a = Rope.from("hello.");
-  a = a.edit(Range(5, 6), Rope.from("!"), RopeInfo.computeInfo, Rope.fromLeaf);
+  RopeNode a = Rope.from('hello.');
+  a = a.edit(Range(5, 6), Rope.from('!'), RopeInfo.computeInfo, Rope.fromLeaf);
   // if we use 1000000, it's too slow while xi-rope is very fast.
   for (var i in Iterable<int>.generate(100000)) {
     var l = a.len();
-    a = a.edit(Range(l, l), Rope.from(i.toString() + "\n"), RopeInfo.computeInfo, Rope.fromLeaf);
+    a = a.edit(Range(l, l), Rope.from(i.toString() + '\n'), RopeInfo.computeInfo, Rope.fromLeaf);
   }
   var l = a.len();
   for (var s in a.clone().chunks(Range(1000, 3000))) {
-    print("chunk $s");
+    print('chunk $s');
   }
-  a = a.edit(Range(1000, l), Rope.from(""), RopeInfo.computeInfo, Rope.fromLeaf);
+  a = a.edit(Range(1000, l), Rope.from(''), RopeInfo.computeInfo, Rope.fromLeaf);
   s.stop();
-  print("elapsed time: ${s.elapsedMilliseconds} ms");
+  print('elapsed time: ${s.elapsedMilliseconds} ms');
   print(a.toStr());
 }
