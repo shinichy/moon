aa701df914a758a6e503d380997a2e1bd8a99462
Shinichi Katayama <shin1.katayama@gmail.com>
2023-05-07 20:57:40 -0700

    Remove flutter code

--- android/app/src/main/kotlin/com/example/moon/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moon
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- client/lib/client.dart
+++
@@ -1,7 +0,0 @@
-export 'src/client.dart';
-export 'src/core_impl.dart';
-export 'src/core_interface.dart';
-export 'src/handler_adapter.dart';
-export 'src/handler_interface.dart';
-export 'src/view_impl.dart';
-export 'src/view_interface.dart';
